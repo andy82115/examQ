340c2ae4687ee9b26457b10536b8116b2020f513
andyyeh <andy82115@gmail.com>
2020-06-10 17:11:13 +0800

    make data binding for MainActivity

--- app/src/main/java/com/andyyeh/examQ/mainActivity/MainActivity.kt
+++ app/src/main/java/com/andyyeh/examQ/mainActivity/MainActivity.kt
@@ -3,7 +3,9 @@ package com.andyyeh.examQ.mainActivity
 import android.os.Bundle
 import android.transition.Slide
 import android.view.Gravity
+import androidx.databinding.DataBindingUtil
 import com.andyyeh.examQ.R
+import com.andyyeh.examQ.databinding.ActivityMainBinding
 import com.andyyeh.examQ.mvvmBase.BaseActivity
 import javax.inject.Inject
 
@@ -11,10 +13,11 @@ class MainActivity : BaseActivity() {
 
     @Inject lateinit var mViewModel: MainActivityViewModel
 
+    private lateinit var dataBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         this.setTransition()
     }
 
